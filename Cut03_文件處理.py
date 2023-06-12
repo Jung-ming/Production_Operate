4c2a3a963c3ec1c1f319ecc7db66fd0960baf1d9
Jung-ming <96611097+Jung-ming@users.noreply.github.com>
2023-06-12 23:41:23 +0800

    v2.1 優化
    可自動輸出檔案至使用者桌面，並且檔名增加日期與時間

--- Cut03_文件處理.py
+++ Cut03_文件處理.py
@@ -1,21 +1,59 @@
 """ 接收Cut01與02程式碼，用來處理讀進來的文件 """
 from Cut01_剪下去_DIP import *
 from Cut02_剪下去_SMT import *
+from Paste01_貼上去_DIP import *
+from Paste02_貼上去_SMT import *
 import pandas as pd
+import os
+import platform
+import datetime
 
 
-def 文件處理(文件路徑, 目標日期):
+def 獲取桌面路徑():
+    # 獲取使用者電腦系統 windows Linux等
+    系統 = platform.system()
+    目錄 = os.path.expanduser("~")
+
+    if 系統 == "Windows":
+        return os.path.join(目錄, "Desktop").replace('\\', '/')
+    elif 系統 == "Darwin":  # macOS
+        return os.path.join(目錄, "Desktop").replace('\\', '/')
+    elif 系統 == "Linux":
+        return os.path.join(目錄, "Desktop").replace('\\', '/')
+    else:
+        # 默認返回用戶主目錄
+        return 目錄.replace('\\', '/')
+
+
+def 文件處理_剪下去(文件路徑, 目標日期):
     data_DIP = pd.read_excel(f'{文件路徑}', header=1, sheet_name='DIP')
     data_SMT = pd.read_excel(f'{文件路徑}', header=1, sheet_name='SMT')
     data_DIP = 剪下去_for_DIP(data_DIP, 目標日期)
     data_SMT = 剪下去_for_SMT(data_SMT, 目標日期)
-    # data_DIP.to_excel('結果_DIP.xlsx', index=False)
-    # data_SMT.to_excel('結果_SMT.xlsx', index=False)
 
-    # {資料夾路徑}/{輸出檔名}
-    writer = pd.ExcelWriter(f'//file-server/生管部/五股廠/Jimmy/剪下結果.xlsx', engine='xlsxwriter')
+    桌面路徑 = 獲取桌面路徑()
+    當天日期_文字格式 = datetime.datetime.now().strftime('%y%m%d%H%M')
+    輸出檔名 = '剪下結果' + 當天日期_文字格式 + '.xlsx'
+    writer = pd.ExcelWriter(f'{桌面路徑}/{輸出檔名}', engine='xlsxwriter')
 
     data_DIP.to_excel(writer, index=False, sheet_name='DIP')
     data_SMT.to_excel(writer, index=False, sheet_name='SMT')
 
-    writer.save()
+    writer.close()
+
+
+def 文件處理_貼上去(文件路徑, 起始日期, 結束日期):
+    data_DIP = pd.read_excel(f'{文件路徑}', header=1, sheet_name='DIP')
+    data_SMT = pd.read_excel(f'{文件路徑}', header=1, sheet_name='SMT')
+    data_DIP = 貼上去_for_DIP(data_DIP, 起始日期, 結束日期)
+    data_SMT = 貼上去_for_SMT(data_SMT, 起始日期, 結束日期)
+
+    桌面路徑 = 獲取桌面路徑()
+    當天日期_文字格式 = datetime.datetime.now().strftime('%y%m%d%H%M')
+    輸出檔名 = '貼上結果' + 當天日期_文字格式 + '.xlsx'
+    writer = pd.ExcelWriter(f'{桌面路徑}/{輸出檔名}', engine='xlsxwriter')
+
+    data_DIP.to_excel(writer, index=False, sheet_name='DIP')
+    data_SMT.to_excel(writer, index=False, sheet_name='SMT')
+
+    writer.close()
